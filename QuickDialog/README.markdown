95296883ffbc90fcee62808145f4c283d0d2fa17
Matthew Knippen <matthew@zwiffer.com>
2012-04-29 03:06:18 -0500

    QuickDialog SDK

---
+++ QuickDialog/README.markdown
@@ -0,0 +1,20 @@
+# QuickDialog
+
+**Quick and simple dialogs for iOS developers.**
+
+## For more information and documentation, please go to [the project official website](http://escoz.com/open-source/quickdialog).
+
+
+QuickDialog allows you to create HIG-compliant iOS forms for your apps  without having to directly deal with UITableViews, delegates and data sources. Fast and efficient, you can create forms with multiple text fields, or with thousands of items with no sweat!
+
+![Sample](https://github.com/escoz/QuickDialog/raw/master/other/quickdialog2.png "Sample")
+![Styling QuickDialog](https://github.com/escoz/QuickDialog/raw/master/other/quickdialog3.png "Styling cells with QuickDialog")
+
+
+Download the project and run the demo app, I'm sure you'll like how simple it is to create powerful dialogs!
+
+----------
+
+*QuickDialog is inspired by the brilliant MonoTouch.Dialog library created by Miguel de Icaza, which can be found at https://github.com/migueldeicaza/MonoTouch.Dialog.*
+
+----------
---
+++ QuickDialog/sample/ExampleViewController.m
@@ -0,0 +1,52 @@
+//                                
+// Copyright 2011 ESCOZ Inc  - http://escoz.com
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License. You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed under
+// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
+// ANY KIND, either express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+
+#import "ExampleViewController.h"
+
+@implementation ExampleViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+	    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+	} else {
+	    return YES;
+	}
+}
+
+-(void)handleWebElementControllerAction:(QWebElement *)element {
+    NSLog(@"Web element selected for url %@", element.url);
+}
+
+-(void)handleChangeEntryExample:(QButtonElement *) button {
+    QEntryElement *entry  = (QEntryElement *) [self.root elementWithKey:@"entryElement"];
+    entry.textValue = @"Hello";
+    [self.quickDialogTableView reloadCellForElements:entry, nil];
+
+}
+
+-(void)exampleAction:(QElement *)element{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Hey!" message:@"This is the exampleAction method in the ExampleViewController" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
+}
+
+
+@end
